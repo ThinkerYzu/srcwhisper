0ae3f0351c51fa8d5493a2945e2cf456db0b3542
Thinker Lee <thinker.li@gmail.com>
2022-04-26 22:12:48 -0700

    Make email mandated in the sign-up form.
    Signed-off-by: Thinker Lee <thinker.li@gmail.com>

--- whisper/register.py
+++ whisper/register.py
@@ -2,6 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
+
+from django.forms import EmailField
+from django.contrib.auth.models import User
+
+
+class UserCreationForm(UserCreationForm):
+    email = EmailField(label="Email address", required=True,
+                       help_text="Required.")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+        def save(self, commit=True):
+            user = super(UserCreationForm, self).save(commit=False)
+            user.email = self.cleaned_data["email"]
+            if commit:
+                user.save()
+                pass
+            return user
+        pass
+    pass
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
